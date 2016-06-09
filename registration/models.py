1326e9f288a2bb2b41cc5cb17bcf983e9b1eeede
hkk710 <hkk710@gmail.com>
2016-06-09 15:21:34 +0530

    24th

--- registration/models.py
+++ registration/models.py
@@ -80,13 +80,13 @@ class StudentManager(models.Manager):
         # do something with the book
         return student
 
-
+course =(('CSE', _('CSE')), ('ME', _('ME')), ('EEE', _('EEE')), ('EC', _('EC')), ('CSA', _('CSA')))
 class Student(models.Model):
     aums_id = models.CharField(_('Aums ID'),  max_length=32, blank=False, unique=True, primary_key=True)
     name = models.CharField(_('First Name'), max_length=32, blank=True, null=True)
     curr_course = models.CharField(_('Current Course'), max_length=32, blank=True, null=True,
                                   validators=[RegexValidator(regex='^[A-Za-z]*$')])
-    branch = models.CharField(_('Branch'), max_length=32, blank=True, null=True,
+    branch = models.CharField(_('Branch'), max_length=32, blank=True,choices= course, null=True,
                                   validators=[RegexValidator(regex='^[A-Za-z]*$')])
     tenth_mark = models.CharField(_('10th Mark'),max_length=5, blank=True, null=True)
     twelth_mark = models.CharField(_('12th Mark'),max_length=5, blank=True, null=True)
