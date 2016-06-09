ac2477e4124adfd92b2a855c20faaef4151612f2
hkk710 <hkk710@gmail.com>
2016-06-09 11:07:08 +0530

    23th

--- registration/urls.py
+++ registration/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
      url(r'^bulk/handle/$', views.handle_student_upload, name='upload_students'),
      url(r'^list/$', StudentListView.as_view(), name='list'),
      url(r'^profile/edit/(?P<aums_id>[\w|\W]+)/$', StudentListUpdateView.as_view(), name='student_profile_update'),
-    ]
+     url(r'^student/filter/external$', TemplateView.as_view(template_name='register/cirstaff/filter_external.html'),name="filter_external"),
+     url(r'^student/filter/internal$', TemplateView.as_view(template_name='register/cirstaff/filter_internal.html'),name="filter_internal"),
+
+]
